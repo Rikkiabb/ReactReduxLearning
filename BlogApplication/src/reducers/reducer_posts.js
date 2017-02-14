fa383c415d5d2c46409f15af3a4fb0982ce87ca8
Rikard Arnar B. Birgisson <rbirgisson@fc-md.umd.edu>
2017-02-14 21:22:56 +0000

    Adds return statement to reducer_posts

--- BlogApplication/src/reducers/reducer_posts.js
+++ BlogApplication/src/reducers/reducer_posts.js
@@ -2,10 +2,10 @@ import { FETCH_POSTS } from '../actions/index';
 
 const INITIAL_STATE = { all: [], post: null };
 
-export default funtion(state = INITIAL_STATE, action){
+export default function(state = INITIAL_STATE, action) {
   switch(action.type){
     case FETCH_POSTS:
-
+      return { ...state, all: action.payload.data };
     default:
       return state;
   }
